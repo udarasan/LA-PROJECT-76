68b30539a0cfa99e610fa0cd9075ea6f77fa1486
Udara San <udarassanjeewa@gmail.com>
2026-02-10 11:22:20 +0530

    introducing entity package

--- src/main/java/com/example/layeredarchitecture/bo/custom/impl/CustomerBOImpl.java
+++ src/main/java/com/example/layeredarchitecture/bo/custom/impl/CustomerBOImpl.java
@@ -4,6 +4,7 @@ import com.example.layeredarchitecture.bo.custom.CustomerBO;
 import com.example.layeredarchitecture.dao.DAOFactory;
 import com.example.layeredarchitecture.dao.custom.CustomerDAO;
 import com.example.layeredarchitecture.dto.CustomerDTO;
+import com.example.layeredarchitecture.enity.Customer;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -14,17 +15,24 @@ public class CustomerBOImpl implements CustomerBO {
             .getDAO(DAOFactory.DAOType.CUSTOMER);
     @Override
     public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
-        return customerDAO.getAll();
+
+        ArrayList<Customer>customers=customerDAO.getAll();
+        ArrayList<CustomerDTO>customerDTOs=new ArrayList<>();
+        for(Customer customer:customers){
+            CustomerDTO customerDTO=new CustomerDTO(customer.getId(),customer.getName(),customer.getAddress());
+            customerDTOs.add(customerDTO);
+        }
+        return customerDTOs;
     }
 
     @Override
     public boolean saveCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
-        return customerDAO.save(customerDTO);
+        return customerDAO.save(new Customer(customerDTO.getId(),customerDTO.getName(),customerDTO.getAddress()));
     }
 
     @Override
     public boolean updateCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
-        return customerDAO.update(customerDTO);
+        return customerDAO.update(new Customer(customerDTO.getId(),customerDTO.getName(),customerDTO.getAddress()));
     }
 
     @Override
